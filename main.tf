f9c5a4bf75b618d79b63db9d5ef4d73a0eee093c
vijender1914 <vijender1914@gmail.com>
2024-10-23 03:02:28 +0530

    psh

--- main.tf
+++ main.tf
@@ -7,7 +7,11 @@ resource "aws_s3_bucket" "r1" {
   
 }
 
+resource "aws_s3_bucket" "r3" {
+    bucket = "iac-aws-checking3"
+  
+}
 resource "aws_s3_bucket" "r2" {
-    bucket = "iac-aws-checking11"
+    bucket = "iac-aws-checking2"
   
 }
